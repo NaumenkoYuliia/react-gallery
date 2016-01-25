53aa4913268db9493772937ee94385caebc69f18
Fernando Marcheselli <fmarcheselli@gmail.com>
2016-01-25 12:41:58 -0800

    update webpack.config, add less dependency

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,6 @@ module.exports = {
   module: {
 
       loaders: [
-            { test: /\.(ttf|eot|svg|woff(2)?)(\?\S*)?$/, loader: 'file-loader'},
             { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader'},
             { test: /\.less$/, loader: "style!css!less" }
         ]
